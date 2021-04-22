ba5d8a3891aa9f6bd5981d925334a0dfac441606
Morgan <morgan.willock@gmail.com>
2021-04-22 20:41:55 +0930

    Added test bootstrap login form with no functinality for testing style.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import DayPickerContainer from './DayPickerContainer';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './Login'
+import BootstrapLogin from './BootstrapLogin'
+import './BootstrapLogin.css'
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Switch>
         <Route exact path="/login-form">
           <Login />
+        </Route>
+        <Route path="/bootstrap-login">
+          <BootstrapLogin />
         </Route>
       </Switch>
     </Router>
